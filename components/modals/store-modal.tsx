ef3452b68d513b2ceb85c489aba26d48fba4a9d6
Istiak Ahmed <mimalahmed6219@gmail.com>
2024-07-16 18:16:40 +0600

    create navbar for admin

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 import * as z from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -15,14 +18,12 @@ import {
 import { Input } from "@/components/ui/input";
 import { Modal } from "@/components/ui/modal";
 import { useStoreModal } from "@/hooks/use-store-modal";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 
 const formSchema = z.object({
   name: z.string().min(1),
 });
+
 export const StoreModal = () => {
   const storeModal = useStoreModal();
 
@@ -38,15 +39,20 @@ export const StoreModal = () => {
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       setLoading(true);
+
       const response = await axios.post("/api/stores", values);
 
+      // this does a complete refresh on our page
+      // sometimes the Next router does not keep things synced with the database
       window.location.assign(`/${response.data.id}`);
     } catch (error) {
       toast.error("Something went wrong.");
+      console.log(error);
     } finally {
       setLoading(false);
     }
   };
+
   return (
     <Modal
       title="Create store"
@@ -54,40 +60,42 @@ export const StoreModal = () => {
       isOpen={storeModal.isOpen}
       onClose={storeModal.onClose}
     >
-      <div className="space-y-4 py-2 pb-4">
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}>
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Name</FormLabel>
-                  <FormControl>
-                    <Input
-                      disabled={loading}
-                      placeholder="E-Commerce"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-              <Button
-                disabled={loading}
-                variant="outline"
-                onClick={storeModal.onClose}
-              >
-                Cancel
-              </Button>
-              <Button disabled={loading} type="submit">
-                Continue
-              </Button>
-            </div>
-          </form>
-        </Form>
+      <div>
+        <div className="space-y-4 py-2 pb-2">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={loading}
+                        placeholder="Enter the name of your store"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                <Button
+                  disabled={loading}
+                  variant="outline"
+                  onClick={storeModal.onClose}
+                >
+                  Cancel
+                </Button>
+                <Button disabled={loading} type="submit">
+                  Continue
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </div>
       </div>
     </Modal>
   );
