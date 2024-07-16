f789a01a9e76362a25114208453e8f5c8d61b900
Istiak Ahmed <mimalahmed6219@gmail.com>
2024-07-16 15:02:31 +0600

    some change and get acctual store id

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,30 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs/server";
+
+import { redirect } from "next/navigation";
+
+export default async function SetupLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  // load the first store available with the current logged in user
+  const store = await prismadb.store.findFirst({
+    where: {
+      userId,
+    },
+  });
+
+  // redirect to the dashboard
+  if (store) {
+    redirect(`/${store.id}`);
+  }
+
+  return <>{children}</>;
+}
